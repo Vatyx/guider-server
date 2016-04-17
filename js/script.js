c9b08c660c4c46aa2ecc13b832b355e18aee9000
Sahil Dhanju <sahildhanju1@gmail.com>
2016-04-17 08:41:39 -0500

    500

--- js/script.js
+++ js/script.js
@@ -81,10 +81,9 @@ $('#div3').click(function() {
   //$.get('/location' function(data) {
   //});
   console.log("I'm in here");
-  $.get('localhost:3000/testing', function(data) {
+  $.get('localhost:5000/testing', function(data) {
     console.log("Got something!");
     console.log(data);
-    alert(data);
 });
 
 });
