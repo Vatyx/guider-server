aceab6cd0e7b17df491da28a16853075bae2644b
Sahil Dhanju <sahildhanju1@gmail.com>
2016-04-17 08:23:57 -0500

    testing

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ $('#traffic').click(function() {
 $('#div3').click(function() {
   //$.get('/location' function(data) {
   //});
-  $.get('https://localhost:3000', function(data) {
+  $.get('https://localhost:3000/testing', function(data) {
     console.log("Got something!");
     console.log(data);
 });
