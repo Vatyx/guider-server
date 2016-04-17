ac555991a8798b88ac804915d12ceeb46b0c8337
Sahil Dhanju <sahildhanju1@gmail.com>
2016-04-17 03:36:02 -0500

    added static

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
 
 var location = {lat: 0, long: 0};
+var speed  
 
 app.get("/", function(req, res) {
 	res.json(location);
